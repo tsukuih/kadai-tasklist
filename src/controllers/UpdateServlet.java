161131d9057623e0018c0bdf2837cb44cd6c6e91
hitoshi.tsukui <tsukuih@gmail.com>
2023-10-23 07:08:02 +0900

    UpdateServet.javaを作成

---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,66 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String _token = request.getParameter("_token");
+
+        if(_token != null && _token.equals(request.getSession().getId())) {
+
+            // DBUtilクラスのcreateEntityManager()オブジェクトでデータベースに接続し
+            // EntityManagerのインスタンスを生成し、変数emにアクセス情報を格納
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからTsakaのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Tasks t = em.find(Tasks.class, (Integer)(request.getSession().getAttribute("tasks_id")));
+
+            // フォームの内容を各フィールドに上書き
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setUpdated_at(currentTime);
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープの不要になったデータを削除
+            request.getSession().removeAttribute("tasks_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
